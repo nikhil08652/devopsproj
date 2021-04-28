df6bce34f6f0eef7e05347e5166f2a2514d15860
EC2 Default User <ec2-user@ip-172-31-95-115.ec2.internal>
2021-04-28 12:26:18 +0000

    CICD Demonstration

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>This is modified home</h1>')
+    return HttpResponse('<h1>This is Homepage, demonstrating CICD</h1>')
