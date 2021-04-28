4b8cc44460853b44be77c8031dd12def62a701e0
EC2 Default User <ec2-user@ip-172-31-95-115.ec2.internal>
2021-04-28 11:08:06 +0000

    disabled pylint

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from home import views
 from django.conf import settings
 from django.conf.urls.static import static
-from settings import STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
