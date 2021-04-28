2d5f2ab0e2bd4393e7381ff2f297899c374e3f23
EC2 Default User <ec2-user@ip-172-31-95-115.ec2.internal>
2021-04-28 10:01:49 +0000

    added static files

--- blog/settings.py
+++ blog/settings.py
@@ -116,8 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
